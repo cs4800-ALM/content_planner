1cded2facf59a8c9d14bf9b04ac709f376b09fe4
LindaThao2 <lcthao@cpp.edu>
2023-02-22 21:33:43 -0800

    Linda - Adding Jsoup Grabs All Hyperlinks example

--- src/main/java/com/example/content_planner/ContentPlannerApplication.java
+++ src/main/java/com/example/content_planner/ContentPlannerApplication.java
@@ -15,6 +15,32 @@ public class ContentPlannerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ContentPlannerApplication.class, args);
 
+// Linda - testing out Jsoup lib for A4
+		// This example shows you how to use jsoup to get page’s title and grabs all links from “google.com”.
+		Document doc;
+		try {
+
+			// need http protocol
+			doc = Jsoup.connect("http://google.com").get();
+
+			// get page title
+			String title = doc.title();
+			System.out.println("title : " + title);
+
+			// get all links
+			Elements links = doc.select("a[href]");
+			for (Element link : links) {
+
+				// get the value from href attribute
+				System.out.println("\nlink : " + link.attr("href"));
+				System.out.println("text : " + link.text());
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
