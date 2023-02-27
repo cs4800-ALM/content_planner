75d4686a7182ecd61a71f7d794fcdac30576b36d
Alondra Sanchez <89600410+alondras1@users.noreply.github.com>
2023-02-27 14:17:49 -0800

    Alondra - added html & css pages

--- src/main/java/com/example/content_planner/ContentPlannerApplication.java
+++ src/main/java/com/example/content_planner/ContentPlannerApplication.java
@@ -2,16 +2,6 @@ package com.example.content_planner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.jsoup.*;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.DataNode;
-import org.jsoup.select.Elements;
-import org.apache.commons.math3.fraction.*;
-import com.google.common.base.Joiner;
-
-import java.io.IOException;
 
 
 @SpringBootApplication
@@ -19,41 +9,6 @@ public class ContentPlannerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContentPlannerApplication.class, args);
-
-		// Linda - testing out Jsoup lib for A4
-		// This example shows you how to use jsoup to get page’s title and grabs all links from “google.com”.
-//		Document doc;
-//		try {
-//
-//			// need http protocol
-//			doc = Jsoup.connect("http://google.com").get();
-//
-//			// get page title
-//			String title = doc.title();
-//			System.out.println("title : " + title);
-//
-//			// get all links
-//			Elements links = doc.select("a[href]");
-//			for (Element link : links) {
-//
-//				// get the value from href attribute
-//				System.out.println("\nlink : " + link.attr("href"));
-//				System.out.println("text : " + link.text());
-//
-//			}
-//
-//			// Michael - testing out Google Guava
-//			// Using Guava's joiner tool to recreate the list above
-//			System.out.println("\nNow with Guava\n==============\n");
-//
-//			String[] stringList = new String[links.size()];
-//			int i = 0;
-//			for (Element link : links) {
-//				stringList[i] = "\nlink : " + link.attr("href") + "\ntext : " + link.text();
-//				i++;
-//			}
-//
-//			System.out.println(Joiner.on("\n").skipNulls().join(stringList));
 //
 //			// Alondra - testing commons math3 library for A4
 //			System.out.println("--------------- ALONDRA - COMMONS MATH3 TESTING------------------");
--- src/main/java/com/example/content_planner/ContentPlannerController.java
+++ src/main/java/com/example/content_planner/ContentPlannerController.java
@@ -1,44 +1,57 @@
 package com.example.content_planner;
 
-import java.util.concurrent.atomic.AtomicLong;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+@Controller
 public class ContentPlannerController {
 
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String aName() {
+        return "index.html";
+    }
 
-    @GetMapping("/search/budget")
-    public static String search_food(@RequestParam Integer budget) {
+    @GetMapping("/search/date")
+    public static String search_content(@RequestParam String date) {
 
-        // database using a dictionary for name and price
-        Dictionary<String, Integer> food_db = new Hashtable<>();
-        food_db.put("Sandwich", 5);
-        food_db.put("Hamburger", 10);
-        food_db.put("Hotdog", 2);
-        food_db.put("Pizza", 25);
+        // simple search bar to search through Key: title for the draft of content Value: date mm/dd/yyyy
+        // if date == dateEntered, then list all titles
+        Dictionary<String, String> search_content = new Hashtable<>();
+        search_content.put("Test content title 1", "02/26/2023");  // one
+        search_content.put("Test content title 2", "02/28/2023");
+        search_content.put("Test content title 3", "03/01/2023");
+        search_content.put("Test content title 4", "03/02/2023");
+        search_content.put("Test content title 5", "03/03/2023");
+        search_content.put("Test content title 6", "02/26/2023"); // two
+        search_content.put("Test content title 7", "03/05/2023");
+        search_content.put("Test content title 8", "03/06/2023");
+        search_content.put("Test content title 9", "03/07/2023");
+        search_content.put("Test content title 10","02/26/2023"); // three    - if the user enters the date of 2/26/2023, then 3 different titles should populate
 
-        String result[] = new String[food_db.size()];
+        // arrayList to hold the result that will display
+        ArrayList<String> result = new ArrayList<String>();
 
         // Enumerating the elements of the dictionary
-        Enumeration<String> keys = food_db.keys();
+        Enumeration<String> keys = search_content.keys();
 
-
-        int index = 0;
+        // search the dictionary to see if the date that the user entered matches anything in the database
         while (keys.hasMoreElements()){
             String key = keys.nextElement();
-            int value = food_db.get(key);
+            String value = search_content.get(key);
 
-            if(value <= budget){
-                result[index] = key;
-                index++;
+            // if the date in the db matches the date that the user entered, then add this to the array list
+            if(value.contentEquals(date)){
+                result.add("Title: " + key + " | " + "Date to Post: " + value);
             }
 
         }
@@ -70,6 +83,8 @@ public class ContentPlannerController {
         return jsonStr;
     }
 
+
+
 //    private static final String template = "Hello, %s!";
 //    private final AtomicLong counter = new AtomicLong();
 //
@@ -77,4 +92,5 @@ public class ContentPlannerController {
 //    public ContentPlanner greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 //        return new ContentPlanner(counter.incrementAndGet(), String.format(template, name));
 //    }
+
 }
