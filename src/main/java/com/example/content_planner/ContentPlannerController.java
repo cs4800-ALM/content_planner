32e63f59bd68978c8ea9ed41628fdc2984520a6c
LindaThao2 <lcthao@cpp.edu>
2023-02-27 20:54:16 -0800

    Linda - added search function

--- src/main/java/com/example/content_planner/ContentPlannerController.java
+++ src/main/java/com/example/content_planner/ContentPlannerController.java
@@ -1,96 +1,98 @@
 package com.example.content_planner;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Hashtable;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ContentPlannerController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/demo")
+//    @RequestMapping(method = RequestMethod.GET, value = "/demo")
+    @GetMapping("/")
     public String aName() {
         return "index.html";
     }
 
-    @GetMapping("/search/date")
-    public static String search_content(@RequestParam String date) {
+    @PostMapping("/register")
+    public String contentSearch(@ModelAttribute ContentPlanner contentPlanner, Model model) {
 
-        // simple search bar to search through Key: title for the draft of content Value: date mm/dd/yyyy
-        // if date == dateEntered, then list all titles
-        Dictionary<String, String> search_content = new Hashtable<>();
-        search_content.put("Test content title 1", "02/26/2023");  // one
-        search_content.put("Test content title 2", "02/28/2023");
-        search_content.put("Test content title 3", "03/01/2023");
-        search_content.put("Test content title 4", "03/02/2023");
-        search_content.put("Test content title 5", "03/03/2023");
-        search_content.put("Test content title 6", "02/26/2023"); // two
-        search_content.put("Test content title 7", "03/05/2023");
-        search_content.put("Test content title 8", "03/06/2023");
-        search_content.put("Test content title 9", "03/07/2023");
-        search_content.put("Test content title 10","02/26/2023"); // three    - if the user enters the date of 2/26/2023, then 3 different titles should populate
+        System.out.println(contentPlanner.toString());
 
-        // arrayList to hold the result that will display
-        ArrayList<String> result = new ArrayList<String>();
-
-        // Enumerating the elements of the dictionary
-        Enumeration<String> keys = search_content.keys();
-
-        // search the dictionary to see if the date that the user entered matches anything in the database
-        while (keys.hasMoreElements()){
-            String key = keys.nextElement();
-            String value = search_content.get(key);
-
-            // if the date in the db matches the date that the user entered, then add this to the array list
-            if(value.contentEquals(date)){
-                result.add("Title: " + key + " | " + "Date to Post: " + value);
-            }
-
-        }
-
-        // Creating Object of ObjectMapper define in Jackson
-        // Api
-        ObjectMapper Obj = new ObjectMapper();
-
-        String jsonStr = null;
-
-        // Try block to check for exceptions
-        try {
-
-            // Getting organisation object as a json string
-            jsonStr = Obj.writeValueAsString(result);
-
-            // Displaying JSON String on console
-            System.out.println(jsonStr);
-        }
-
-        // Catch block to handle exceptions
-        catch (IOException e) {
-
-            // Display exception along with line number
-            // using printStackTrace() method
-            e.printStackTrace();
-        }
-
-        return jsonStr;
+        // validate
+        System.out.println(contentPlanner.getTitle());
+        System.out.println(contentPlanner.getDate());
+        model.addAttribute("title", contentPlanner.getTitle());
+        model.addAttribute("date", contentPlanner.getDate());
+        return "index";
     }
 
 
-
-//    private static final String template = "Hello, %s!";
-//    private final AtomicLong counter = new AtomicLong();
+//    @GetMapping("/search/date")
+//@RequestMapping("/search/date")
+//@RequestMapping(value = "/search/date", method = RequestMethod.GET)
+//@ResponseBody
+//    public static String search_content(@RequestParam String date) {
 //
-//    @GetMapping("/greeting")
-//    public ContentPlanner greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-//        return new ContentPlanner(counter.incrementAndGet(), String.format(template, name));
+//
+//        // simple search bar to search through Key: title for the draft of content Value: date mm/dd/yyyy
+//        // if date == dateEntered, then list all titles
+//        Dictionary<String, String> search_content = new Hashtable<>();
+//        search_content.put("Test content title 1", "02/26/2023");  // one
+//        search_content.put("Test content title 2", "02/28/2023");
+//        search_content.put("Test content title 3", "03/01/2023");
+//        search_content.put("Test content title 4", "03/02/2023");
+//        search_content.put("Test content title 5", "03/03/2023");
+//        search_content.put("Test content title 6", "02/26/2023"); // two
+//        search_content.put("Test content title 7", "03/05/2023");
+//        search_content.put("Test content title 8", "03/06/2023");
+//        search_content.put("Test content title 9", "03/07/2023");
+//        search_content.put("Test content title 10","02/26/2023"); // three    - if the user enters the date of 2/26/2023, then 3 different titles should populate
+//
+//        // arrayList to hold the result that will display
+//        ArrayList<String> result = new ArrayList<String>();
+//
+//        // Enumerating the elements of the dictionary
+//        Enumeration<String> keys = search_content.keys();
+//
+//        // search the dictionary to see if the date that the user entered matches anything in the database
+//        while (keys.hasMoreElements()){
+//            String key = keys.nextElement();
+//            String value = search_content.get(key);
+//
+//            // if the date in the db matches the date that the user entered, then add this to the array list
+//            if(value.contentEquals(date)){
+//                result.add("Title: " + key + " | " + "Date to Post: " + value);
+//            }
+//
+//        }
+//
+//        // Creating Object of ObjectMapper define in Jackson
+//        // Api
+//        ObjectMapper Obj = new ObjectMapper();
+//
+//        String jsonStr = null;
+//
+//        // Try block to check for exceptions
+//        try {
+//
+//            // Getting organisation object as a json string
+//            jsonStr = Obj.writeValueAsString(result);
+//
+//            // Displaying JSON String on console
+//            System.out.println(jsonStr);
+//        }
+//
+//        // Catch block to handle exceptions
+//        catch (IOException e) {
+//
+//            // Display exception along with line number
+//            // using printStackTrace() method
+//            e.printStackTrace();
+//        }
+//
+//        return jsonStr;
 //    }
 
 }
