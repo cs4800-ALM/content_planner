edb4713250f72ce850607dc31e2073b7fc2012c6
LindaThao2 <lcthao@cpp.edu>
2023-05-01 23:09:20 -0700

    Linda - updates so we can connect to aws

--- backend/src/main/java/com/example/content_planner/controller/PostsController.java
+++ backend/src/main/java/com/example/content_planner/controller/PostsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://3.145.69.142:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class PostsController {
